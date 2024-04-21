ed6866931bc802a6c1633d7dffe5dcd6d07cae55
bissergey <nurixayt@mail.ru>
2024-04-21 13:36:03 +0700

    Разработана логика приложения: Note, Project, NoteCategories Добавлена библиотека Newtonsoft JSON.NET. Добавлен класс сериализации

--- NoteAppUI/Form1.Designer.cs
+++ NoteAppUI/Form1.Designer.cs
@@ -30,13 +30,58 @@ namespace NoteAppUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(464, 95);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(112, 30);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(327, 99);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 22);
+            this.nameBox.TabIndex = 1;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(464, 144);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(112, 28);
+            this.DownloadButton.TabIndex = 2;
+            this.DownloadButton.Text = "Загрузить";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button DownloadButton;
     }
 }
 
--- NoteAppUI/Form1.cs
+++ NoteAppUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NoteApp;
 
 namespace NoteAppUI
 {
@@ -15,6 +16,44 @@ namespace NoteAppUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = nameBox.Text; // Получение данных из nameBox
+
+            // Проверка, хранились ли в nameBox данные
+            if (!string.IsNullOrEmpty(name))
+            {
+                Note note = new Note();
+                note.Name = name;
+
+                // Сохранение данных в файл
+                Project.Notes.Add(note);
+                ProjectManager.SaveToFile(Project.Notes, "notes.json");
+
+                MessageBox.Show("Данные успешно сериализованы и сохранены в notes.json");
+            }
+            else
+            {
+                MessageBox.Show("Введите название в nameBox");
+            }
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            // Загрузка данных из файла
+            Project.Notes = ProjectManager.LoadFromFile("notes.json");
+
+            if (Project.Notes.Count > 0)
+            {
+                Note loadedNote = Project.Notes[0];
+                MessageBox.Show($"Название из загруженного объекта: {loadedNote.Name}");
+            }
+            else
+            {
+                MessageBox.Show("Отсутствуют данные для десериализации");
+            }
         }
     }
 }
